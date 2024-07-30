173881c2cbc66cb870d60b3f1e0a376163fed4ec
Assaf <assafyechiel@gmail.com>
2024-07-30 19:22:16 +0300

    Create gameBoard IIFE

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const rows = 3;
-const columns = 3;
-const board = [];
-
-let k = 1;
-
-for (i = 0; i < rows; i++) {
-    board[i] = [];
-    for (j = 0; j < columns; j++) {
-        board[i].push(k);
-        // console.log(k);
-        k++;
-    };
-    console.log(board[i].join(" "));
-}
+const gameBoard = (() => {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    
+    let k = 1;
+    
+    for (i = 0; i < rows; i++) {
+        board[i] = [];
+        for (j = 0; j < columns; j++) {
+            board[i].push(k);
+            // console.log(k);
+            k++;
+        };
+        console.log(board[i].join(" "));
+    }    
+})();
