a3274e57c8cbb5396ced419704fdfe4c7b7c7a02
Assaf <assafyechiel@gmail.com>
2024-08-24 13:35:36 +0300

    Update script comments

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const gameBoard = () => {
     const columns = 3;
     const board = [];
     let k = 0;
-        
+    
+    // 2D array loop
     for (i = 0; i < rows; i++) {
         board[i] = [];
         for (j = 0; j < columns; j++) {
-            board[i].push(k);
+            board[i].push(k); // Allocates numbers to cells
             k++;
         }
     }
 
+    // Sends board array
     const getBoard = () => newBoard = board;
 
+    // Allocates player's piece to chosen cell
     const dropPiece = (cell, player) => {
         const availableCells = board.filter((row) => row.map((cell) => cell));
 
@@ -26,6 +29,7 @@ const gameBoard = () => {
         });
     }
 
+    // Prints updates board array
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell));
 
@@ -45,13 +49,14 @@ const gameController = (
     playerOneName = "Player One",
     playerTwoName = "Player Two"
     ) => {
-
-    const board = gameBoard();
+    // Creates gameBoard function variable and executes gameBoard factory function
+    const board = gameBoard(); 
     turnCounter = 0;
     let playerWinCheck = false;
     let playerTieCheck = false;
 
-    const players   = [
+    // Creates array of player details
+    const players = [
         {
             name: playerOneName,
             piece: "X"
@@ -64,38 +69,46 @@ const gameController = (
 
     let activePlayer = players[0];
 
+    // Switches current player
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
+    // Sends current player
     const getActivePlayer = () => activePlayer;
 
+    // Prints updated board array of each round
     const printNewRound = () => {
         board.printBoard();
         console.log(`${getActivePlayer().name}'s turn.`);
     }
 
+
     const playRound = (cell) => {
-        if (cell === "X" || cell === "O") {
+        // Checks if player selection already contains a player value
+        if (cell === "X" || cell === "O") { 
             console.log("Cell already taken\nMake another move");
             return;
-        } else if (cell.length > 1) {
+        } // Checks if player selection is a valid move
+        else if (cell.length > 1) { 
             console.log("Invalid move\nTry again");
             return;
-        } else {
+        } // Executes player selection
+        else { 
             console.log(`Dropping ${getActivePlayer().name}'s token into cell ${cell}...`);
             board.dropPiece(cell, getActivePlayer().piece);
         }
 
+        // Brings board array in current function scope
         board.getBoard();
 
-        // Horizontal win check
+        // Checks for horizontal win
         if (newBoard[0][0] == getActivePlayer().piece && newBoard[0][1] == getActivePlayer().piece && newBoard[0][2] == getActivePlayer().piece ||
             newBoard[1][0] == getActivePlayer().piece && newBoard[1][1] == getActivePlayer().piece && newBoard[1][2] == getActivePlayer().piece ||
             newBoard[2][0] == getActivePlayer().piece && newBoard[2][1] == getActivePlayer().piece && newBoard[2][2] == getActivePlayer().piece
         ) {
             playerWinCheck = true;
         } 
-        // Vertical win check
+        // Checks for vertical win
         else if (
             newBoard[0][0] == getActivePlayer().piece && newBoard[1][0] == getActivePlayer().piece && newBoard[2][0] == getActivePlayer().piece ||
             newBoard[0][1] == getActivePlayer().piece && newBoard[1][1] == getActivePlayer().piece && newBoard[2][1] == getActivePlayer().piece ||
@@ -103,14 +116,14 @@ const gameController = (
         ) {
             playerWinCheck = true;            
         } 
-        // Diagonal win check
+        // Checks for diagonal win
         else if (
             newBoard[0][0] == getActivePlayer().piece && newBoard[1][1] == getActivePlayer().piece && newBoard[2][2] == getActivePlayer().piece ||
             newBoard[0][2] == getActivePlayer().piece && newBoard[1][1] == getActivePlayer().piece && newBoard[2][0] == getActivePlayer().piece 
         ) {
             playerWinCheck = true;            
         } 
-        // Tie check
+        // Checks for tie
         else {
             turnCounter++;
             if (turnCounter == 9) {
@@ -123,10 +136,13 @@ const gameController = (
             }
         } 
         
+        // Identifies no winner yet
+        // Switches players & updates round
         if (playerWinCheck === false) {
             switchPlayerTurn();
             printNewRound();
-        } else if (playerWinCheck === true) {
+        } // Identifies winner and ends function execution
+        else if (playerWinCheck === true) {
             for (i = 0; i < 3; i++) {
                 console.log(newBoard[i]);
             }
@@ -136,8 +152,10 @@ const gameController = (
         } 
     }
 
+    // Sends win check variable
     const getWinCheck = () => playerWinCheck;
 
+    // Sends tie check variable
     const getTieCheck = () => playerTieCheck;
 
     return {
@@ -151,17 +169,21 @@ const gameController = (
 }
 
 const screenController = (() => {
+    // Creates gameController function variable and executes gameBoard factory function
     const game = gameController();
     const playerTurnDiv = document.querySelector('.turn');
     const boardDiv = document.querySelector('.board');
 
+    // Brings board array into current function
     const board = game.getBoard();
 
+    // Refreshes board with updated selection
     const updateScreen = () => {
         boardDiv.textContent = "";
         const activePlayer = game.getActivePlayer();
         playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
 
+        // Creates button element for each cell
         board.forEach(row => {
             row.forEach((cell, index) => {
                 const cellButton = document.createElement('button');
@@ -175,11 +197,14 @@ const screenController = (() => {
         })
     }
 
+    // Adds event listener to board
     const clickHandlerBoard = (() => {
         boardDiv.addEventListener('click', (e) => {
             
-           if (game.getWinCheck() === true || game.getTieCheck() === true) return;
+            // Stops event listener once game over
+            if (game.getWinCheck() === true || game.getTieCheck() === true) return;
 
+            // 'Sees' selected cell
             const selectedCell = e.target.innerText;
 
             game.playRound(selectedCell);
