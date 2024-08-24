c461ef35eca4bcc6bb434990f3d56a97164dea8e
Assaf <assafyechiel@gmail.com>
2024-08-24 15:09:46 +0300

    Create & display game status variable

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ const gameController = (
     ) => {
     // Creates gameBoard function variable and executes gameBoard factory function
     const board = gameBoard(); 
+    
+    let gameStatus;
     turnCounter = 0;
     let playerWinCheck = false;
     let playerTieCheck = false;
@@ -87,15 +89,17 @@ const gameController = (
         // Checks if player selection already contains a player value
         if (cell === "X" || cell === "O") { 
             console.log("Cell already taken\nMake another move");
-            return;
+            return gameStatus = "Cell already taken\nMake another move";
+            // return;
         } // Checks if player selection is a valid move
         else if (cell.length > 1) { 
             console.log("Invalid move\nTry again");
-            return;
+            return gameStatus = "Invalid move\nTry again";
         } // Executes player selection
         else { 
             console.log(`Dropping ${getActivePlayer().name}'s token into cell ${cell}...`);
             board.dropPiece(cell, getActivePlayer().piece);
+            gameStatus = "";
         }
 
         // Brings board array in current function scope
@@ -132,7 +136,8 @@ const gameController = (
                 }
                 console.log("It's a tie.");
                 console.log("Game Over");
-                return playerTieCheck = true;
+                playerTieCheck = true;
+                return gameStatus = "It's a tie.";
             }
         } 
         
@@ -148,9 +153,12 @@ const gameController = (
             }
             console.log(`${getActivePlayer().name} wins`);
             console.log("Game Over");
-            return;
+            return gameStatus = `${getActivePlayer().name} wins`;
         } 
     }
+
+    // Sends game status variable
+    const getGameStatus = () => gameStatus;
 
     // Sends win check variable
     const getWinCheck = () => playerWinCheck;
@@ -163,25 +171,37 @@ const gameController = (
         printNewRound,
         playRound,
         getBoard: board.getBoard,
+        getGameStatus,
         getWinCheck,
         getTieCheck
     };
 }
 
 const screenController = (() => {
+    const boardDiv = document.querySelector('.board');
+    const playerTurnDiv = document.querySelector('.turn');
+    const gameStatusDiv = document.querySelector('.game-status');
+
     // Creates gameController function variable and executes gameBoard factory function
     const game = gameController();
-    const playerTurnDiv = document.querySelector('.turn');
-    const boardDiv = document.querySelector('.board');
-
     // Brings board array into current function
     const board = game.getBoard();
+
+    let gameStatusTimer;
 
     // Refreshes board with updated selection
     const updateScreen = () => {
         boardDiv.textContent = "";
         const activePlayer = game.getActivePlayer();
-        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+        if (game.getWinCheck() === true || game.getTieCheck() === true) {
+            playerTurnDiv.textContent = "";    
+        } else {
+            playerTurnDiv.textContent = `${activePlayer.name}'s turn...`; // Displays current player's name
+        }
+        gameStatusDiv.textContent = game.getGameStatus(); // Displays current game status
+        gameStatusTimer = setTimeout(() => { // Sets game status timeout
+            gameStatusDiv.textContent = "";
+        }, 3000);
 
         // Creates button element for each cell
         board.forEach(row => {
@@ -201,6 +221,9 @@ const screenController = (() => {
     const clickHandlerBoard = (() => {
         boardDiv.addEventListener('click', (e) => {
             
+            // Clears timeout of game status display
+            clearTimeout(gameStatusTimer);
+
             // Stops event listener once game over
             if (game.getWinCheck() === true || game.getTieCheck() === true) return;
 
